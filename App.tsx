7c5c7db55e78dfbbfae4969be103aebd91faec86
ChavinduShirantha <chavindushirantha@gmail.com>
2024-03-06 15:53:17 +0530

    Created Search bar

--- App.tsx
+++ App.tsx
@@ -5,17 +5,32 @@
  * @format
  */
 
-import React from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import React, { useCallback, useState } from "react";
+import {ImageBackground, StyleSheet, TextInput, View} from 'react-native';
 
+import axios from "axios";
 const App = () => {
+  const [input,setInput] = useState('');
+  const [loading,setLoading] = useState(false);
+  const [data,setData] = useState([]);
+  const fetchDataHandler = useCallback(() => {}, []);
   return (
     <View style={styles.root}>
       <ImageBackground
         source={require('./assets/bg.jpg')}
         resizeMode="cover"
-        style={styles.image}
-      />
+        style={styles.image}>
+        <View>
+          <TextInput
+            placeholder="Enter Location here ..."
+            onChangeText={text => setInput(text)}
+            value={input}
+            placeholderTextColor={'#000'}
+            style={styles.textInput}
+            onSubmitEditing={fetchDataHandler}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -24,9 +39,21 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
-  image:{
+  image: {
     flex: 1,
     flexDirection: 'column',
-  }
+  },
+  textInput: {
+    borderBottomWidth: 4,
+    padding: 10,
+    paddingVertical: 20,
+    marginVertical: 100,
+    marginHorizontal: 20,
+    backgroundColor: '#fff',
+    fontSize: 16,
+    borderRadius: 15,
+    borderBottomColor: 'rgb(0,69,162)',
+  },
 });
+
 export default App;
